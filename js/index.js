9efdb35834b6899fd0a6e8d53f407313a45ba674
onecampus-liudongyu <liudongyu@thecampus.cc>
2015-05-21 11:19:24 +0800

    add front files

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+		setScreenClass();
+});
+$(window).resize(function(){
+		setScreenClass();
+});
+
+/**获取屏幕宽度*/
+function getBrowserWidth(){
+		if (window.innerWidth){
+				return window.innerWidth;}
+		else if (document.documentElement && document.documentElement.clientWidth != 0){
+				return document.documentElement.clientWidth;	}
+		else if (document.body){return document.body.clientWidth;}
+		return 0;
+}
+
+/**获取屏幕高度*/
+function getBrowserHeight(){
+		if (window.innerHeight){
+				return window.innerHeight;}
+		else if (document.documentElement && document.documentElement.clientHeight != 0){
+				return document.documentElement.clientHeight;	}
+		else if (document.body){return document.body.clientHeight;}
+		return 0;
+}
+
+/**动态设置页面相关的css属性*/
+function setScreenClass(){
+		var sw = getBrowserWidth();
+		if(sw>=960)(sw=960);
+
+		/**设置head背景图高度*/
+		$(".head").css("height",sw/2.7 + "px");
+}
